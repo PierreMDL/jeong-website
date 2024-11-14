ae981e5e5c5ee02fa3e06a68f3adad3bf9482ef5
PierreMDL <pierresimon.mdl@gmail.com>
2024-11-14 21:16:24 +0000

    fix: get db url env from dynamic

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { createPool } from '@vercel/postgres';
-import { POSTGRES_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export function addLocation(sessionId: string, lat: number, long: number, isGpsLocation: boolean, message: string | undefined, tagNumber: number | undefined): void {
-    const databasePool = createPool({ connectionString: POSTGRES_URL });
+    const databasePool = createPool({ connectionString: env.POSTGRES_URL });
     databasePool.connect().then(() => {
         databasePool.sql`INSERT INTO entries (session_id, is_gps, latitude, longitude, message, tag_number) VALUES (${sessionId}, ${isGpsLocation}, ${lat}, ${long}, ${message}, ${tagNumber})`;
     }).catch(() => {
