ef065218b82a7602c3c0d5e6efd0f45c2de73ed6
PierreMDL <pierresimon.mdl@gmail.com>
2024-11-14 21:55:23 +0000

    fix: log that db insertion went ok

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,9 @@ import { env } from '$env/dynamic/private';
 export function addLocation(sessionId: string, lat: number, long: number, isGpsLocation: boolean, message: string | undefined, tagNumber: number | undefined): void {
     const databasePool = createPool({ connectionString: env.POSTGRES_URL });
     databasePool.connect().then(() => {
-        databasePool.sql`INSERT INTO entries (session_id, is_gps, latitude, longitude, message, tag_number) VALUES (${sessionId}, ${isGpsLocation}, ${lat}, ${long}, ${message}, ${tagNumber})`;
+        databasePool.sql`INSERT INTO entries (session_id, is_gps, latitude, longitude, message, tag_number) VALUES (${sessionId}, ${isGpsLocation}, ${lat}, ${long}, ${message}, ${tagNumber})`.then((res) => {
+            console.log("Ok");
+        });
     }).catch(() => {
         console.log("Adding location did not succeed");
     }).finally(() => {
